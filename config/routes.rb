3c58b5303a5794e2fff1ef5b5b72166e14c421cc
Pablo Israel <pisrael@medallia.com>
2019-09-01 14:38:38 -0300

    Add requests for networks and banks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :atms do
   end
+
+  get '/networks', to: 'atms#networks'
+  get '/banks', to: 'atms#banks'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
